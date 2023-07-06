5f59554c6e133e62644593c5c818f6e671c3889f
Phuong Bui <buiphuongaptutc@gmail.com>
2023-07-06 14:18:41 +0700

    product detail

--- src/app/components/product-item-detail/product-item-detail.component.ts
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { concatMap, map, tap } from 'rxjs';
+import { CartService } from 'src/app/services/cart.service';
+import { Product, ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-item-detail',
   templateUrl: './product-item-detail.component.html',
   styleUrls: ['./product-item-detail.component.scss']
 })
-export class ProductItemDetailComponent {
+export class ProductItemDetailComponent implements OnInit {
+  _activatedRoute = inject(ActivatedRoute)
+  _product = inject(ProductService)
+  _cart = inject(CartService)
 
+  product!: Product | undefined
+
+  ngOnInit(): void {
+    this._activatedRoute.params.pipe(
+      map(i => i['id']),
+      concatMap(id => this._product.get(id)),
+      tap(prd => console.log(prd),
+      )
+    ).subscribe(prd => this.product = prd)
+  }
+
+
+  addCart(qty: number) {
+    if (!this.product)
+      return
+    this._cart.add(this.product.id, qty)
+    console.log(this._cart.getAll());
+    alert(`'${this.product.name}' was added to your cart!`)
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 const URL = '/assets/data.json'
 
@@ -20,5 +20,11 @@ export class ProductService {
 
   list(): Observable<Product[]> {
     return this._http.get<Product[]>(URL)
+  }
+
+  get(id: number): Observable<Product | undefined> {
+    return this.list().pipe(
+      map(lst => lst.find(i => i.id == id))
+    )
   }
 }
